7d0d32a9d336ac74fbfa75faab41011a8a157d1d
Chien <chien@gmail.com>
2017-08-27 10:31:10 +0800

    v4

--- app/src/main/java/tw/brad/app/helloworld/myfragmenttest/F1Fragment.java
+++ app/src/main/java/tw/brad/app/helloworld/myfragmenttest/F1Fragment.java
@@ -7,8 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class F1Fragment extends Fragment {
+    private TextView mesg;
+    private View mainView;
+
     public F1Fragment(){
         Log.i("brad", "F1Fragment()");
     }
@@ -26,9 +31,35 @@ public class F1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.i("brad", "F1Fragment:onCreateView()");
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        if (mainView == null) {
+            mainView = inflater.inflate(R.layout.fragment_f1, container, false);
+            mesg = (TextView) mainView.findViewById(R.id.f1mesg);
+            final View f1test1 = mainView.findViewById(R.id.f1test1);
+            f1test1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    f1test1();
+                }
+            });
+            final View f1test2 = mainView.findViewById(R.id.f1test2);
+            f1test2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    f1test2();
+                }
+            });
+        }
+
+        return mainView;
     }
+
+    private void f1test1(){
+        mesg.setText("" + (int)(Math.random()*49+1));
+    }
+    private void f1test2(){
+        
+    }
+
 
     @Override
     public void onAttach(Context context) {
--- app/src/main/java/tw/brad/app/helloworld/myfragmenttest/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/myfragmenttest/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private F1Fragment f1;
     private F2Fragment f2;
     private FragmentManager fmgr;
     private FragmentTransaction tran;
+    private boolean isF2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void change(View view){
+        isF2 = !isF2;
+
+        tran = fmgr.beginTransaction();
+        tran.replace(R.id.container, isF2?f2:f1);
+        //tran.addToBackStack(null);
+        tran.commit();
+    }
+
 }
